f62d9a73ff9b53bc1bace4842b8a15a576647930
Michael Rukevwe Omonedo <105046150+Rukkyoo@users.noreply.github.com>
2025-06-10 21:09:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World, this is my first Node.js server!');
+  res.end();
+}).listen(8080);
+
+console.log("Server running at http://localhost:8080");
+
+
