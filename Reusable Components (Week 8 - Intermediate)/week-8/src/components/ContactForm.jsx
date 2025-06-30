03e94250f50154cbc4a21a6dd79914853b33b3aa
Michael Omonedo <rukkyomonedo@gmail.com>
2025-06-30 13:01:36 +0100

    included link to github

--- Reusable Components (Week 8 - Intermediate)/week-8/src/components/ContactForm.jsx
+++ Reusable Components (Week 8 - Intermediate)/week-8/src/components/ContactForm.jsx
@@ -5,6 +5,16 @@ function ContactForm() {
       <input type="email" placeholder="Your email" required />
       <textarea placeholder="Your message" required />
       <button type="submit">Send</button>
+      <p>
+        Check{" "}
+        <a
+          href="https://github.com/Rukkyoo/Flexisaf-Internship-May-2025-Cohort/tree/main/Reusable%20Components%20(Week%208%20-%20Intermediate)/week-8"
+          target="_blank"
+        >
+          GitHub
+        </a>{" "}
+        Repo
+      </p>
     </form>
   );
 }
--- Reusable Components (Week 8 - Intermediate)/week-8/src/components/Footer.jsx
+++ Reusable Components (Week 8 - Intermediate)/week-8/src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <a href="https://github.com/Rukkyoo" target="_blank">GitHub</a> | 
-      <a href="https://linkedin.com" target="_blank">LinkedIn</a>
+      <a href="http://linkedin.com/in/michael-omonedo/" target="_blank">LinkedIn</a>
     </footer>
   );
 }
